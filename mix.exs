355799895d8ffd869601804e203be7a5b2386217
feng19 <kevin@feng19.com>
2024-12-24 15:15:18 +0800

    update deps

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ExProxyServer.MixProject do
   def project do
     [
       app: @app,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
